1014ccdceb276a3c44a4f2a32423d07dd8cb4e76
Niranjan Kamlakr Aware <niroba.aware.26@gmail.com>
2023-06-16 23:42:42 +0530

    sidebar css

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -13,15 +13,16 @@ function LandingPage() {
           </ul>
         </nav>
       </div>
-      <div>
-      <nav>
+      <div className={classes.mainsidebar}>
+      <nav className={classes.sidebar_nav}>
         <div className={classes.sidebar}>
-            <button className={classes.toggle_btn}>&#9776;</button>
             <ul className={classes.sidebar_ul}>
-                <li className={classes.sidebar_li}><a className={classes.sidebar_a} href="#">Home</a></li>
+                <li className={classes.sidebar_li}><a className={classes.sidebar_a} href="#">DriverSymb</a></li>
+                <li className={classes.sidebar_li}><a className={classes.sidebar_a} href="#">Carsymb</a></li>
             </ul>
         </div>
     </nav>
+    <p>swiftRIDE</p>
       </div>
     </body>
   );
