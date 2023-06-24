dd05e5a0a87a9fc2b5c47545e0122404129bb6c5
Niranjan Kamlakr Aware <niroba.aware.26@gmail.com>
2023-06-24 23:49:31 +0530

    Auth
    Auth pages added succefully

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { Fragment } from 'react';
-import LandingPage from './components/LandingPage'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.css';
-import Home from './components/Home';
-
+import React from "react";
+import { Fragment } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Home from "./components/Home";
+import { BrowserRouter, Route, Routes, Switch } from "react-router-dom";
+import Login from "./components/Auth/Login";
+import RegisterAsCarOwner from "./components/Auth/RegisterAsCarOwner";
+import RegisterAsDriver from "./components/Auth/RegisterAsDriver";
 
 function App() {
   return (
     <Fragment>
-    
-    
-     <Home/>   
-       {/* <LandingPage/> */}
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route
+            path="/registerascarowner"
+            element={<RegisterAsCarOwner />}
+            exact
+          />
+          <Route
+            path="/registerasdriver"
+            element={<RegisterAsDriver />}
+            exact
+          />
+          <Route path="/login" element={<Login />} exact />
+        </Routes>
+      </BrowserRouter>
     </Fragment>
   );
 }
